08521aeec4499bb003e6c12e1b6113ec570aac33
oyolcinar <oyolcinar@gmail.com>
2022-02-10 20:49:26 +0300

    v.02

---
+++ script.js
@@ -0,0 +1,41 @@
+/* Selectors */
+
+const zero = document.getElementById("zeroBtn");
+const one = document.getElementById("oneBtn");
+const two = document.getElementById("twoBtn");
+const three = document.getElementById("threeBtn");
+const four = document.getElementById("fourBtn");
+const five = document.getElementById("fiveBtn");
+const six = document.getElementById("sixBtn");
+const seven = document.getElementById("sevenBtn");
+const eight = document.getElementById("eightBtn");
+const nine = document.getElementById("nineBtn");
+const add = document.getElementById("addBtn");
+const subtract = document.getElementById("subtractBtn");
+const multiply = document.getElementById("multiplyBtn");
+const divide = document.getElementById("divideBtn");
+const point = document.getElementById("pointBtn");
+const equals = document.getElementById("equalsBtn");
+const cancel = document.getElementById("cBtn");
+const reset = document.getElementById("acBtn");
+const display = document.getElementById("display");
+
+/* Basic Operator Functions */
+
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  return x / y;
+}
+
+/* Display */
